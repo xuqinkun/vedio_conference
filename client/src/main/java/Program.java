7d9f055070f82593eb663e2ac0643cd6d636db51
xuqinkun <15528498250@163.com>
2020-07-18 10:35:04 +0800

    Debug switch

--- client/src/main/java/Program.java
+++ client/src/main/java/Program.java
@@ -3,22 +3,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
+import java.util.List;
 
 public class Program extends Application {
 
-    @Override
-    public void init() throws Exception {
-        super.init();
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        List<String> params = getParameters().getRaw();
+        boolean debugMode = params.size() > 0 && params.get(0).equalsIgnoreCase("-d");
         primaryStage.setTitle("Meeting");
-        Parent root = FXMLLoader.load(
-                getClass().getResource("/fxml/main.fxml"));
-//        Parent root = FXMLLoader.load(
-//                getClass().getResource("/fxml/MeetingRoom.fxml"));
+        Parent root;
+        if (debugMode) {
+            root = FXMLLoader.load(getClass().getResource("/fxml/MeetingRoom.fxml"));
+        } else {
+            root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        }
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(true);
         primaryStage.show();
