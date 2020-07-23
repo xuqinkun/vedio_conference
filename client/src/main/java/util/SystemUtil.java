b3698dd2016138b08d75b6291fe584c6ce0bf45e
xuqinkun <15528498250@163.com>
2020-07-23 11:12:14 +0800

    【Fix Bug】系统通知显示不全问题

--- client/src/main/java/util/SystemUtil.java
+++ client/src/main/java/util/SystemUtil.java
@@ -4,6 +4,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import service.schedule.layout.AnimationControlTask;
@@ -15,13 +16,17 @@ public class SystemUtil {
         Stage infoStage = new Stage();
         AnchorPane ap = new AnchorPane();
         ap.setMinSize(360, 70);
-        ap.setMaxSize(360, 70);
+        ap.setMaxSize(400, 140);
+        ap.setPrefSize(400, 140);
         ap.setStyle("-fx-background-color:  #000000");
         ap.setOpacity(0.9);
 
         Label label = new Label(content);
         label.setAlignment(Pos.CENTER);
         label.setStyle("-fx-text-fill: #ffffff;-fx-font-family: 'Microsoft YaHei';-fx-font-size: 20");
+        label.setPrefSize(400, 70);
+        label.setWrapText(true);
+        label.setTextAlignment(TextAlignment.CENTER);
 
         ap.getChildren().add(label);
         AnchorPane.setLeftAnchor(label, 0.0);
